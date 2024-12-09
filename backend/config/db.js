f2e84aff21d73ce4e0108747e799dfff2fcfc64e
Ritik Sharma <sharmaritik2002@gmail.com>
2024-12-09 18:52:16 +0530

    addition of new products and changes to product details card.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI)
+        const connect = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 30000
+        })
         console.log(`MongoDB Connected: ${connect.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,19 +7,26 @@ const Product = ({product}) => {
   return (
     <LinkContainer to={`/product/${product._id}`}>
         <Card className='my-3 p-3 rounded'>
-        <Card.Img src={product.image} variant='top'></Card.Img>
-        <Card.Body>
-            <Card.Title as='div' className='product-title'>
-                <strong>{product.name}</strong>
-            </Card.Title>
-            <Card.Text>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-            </Card.Text>
-            <Card.Text as='h3'>
-                ${product.price}
-            </Card.Text>
-        </Card.Body>
-    </Card>
+        <div className='overflow-hidden d-flex align-items-center justify-content-center' style={{ height: '250px', width: '100%' }}>
+            <Card.Img 
+                src={product.image} 
+                variant='top' 
+                className='img-fluid' 
+                style={{ objectFit: 'contain', height: '100%', width: '100%' }}
+            />
+        </div>
+            <Card.Body>
+                <Card.Title as='div' className='product-title'>
+                    <strong>{product.name}</strong>
+                </Card.Title>
+                <Card.Text>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                </Card.Text>
+                <Card.Text as='h3'>
+                    ${product.price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     </LinkContainer>
   )
 }
